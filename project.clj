fe691eef50a092c33e714c6511ffeb5045967a25
Marcus Blankenship <marcus@creoagency.com>
2014-09-11 00:25:06 +0000

    new kool card game!

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot clojurefun.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}) 
--- src/clojurefun/core.clj
+++ src/clojurefun/core.clj
@@ -1,7 +1,78 @@
 (ns clojurefun.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn fib [x]
+  (cond
+   (= x 0) []
+   (= x 1) [1]
+   (= x 2) [1 1]
+   :else (let [fib-1 (fib (- x 1))
+               c (count fib-1)
+               fib1 (nth fib-1 (- c 1))
+               fib2 (nth fib-1 (- c 2))]
+           (conj fib-1 (+ fib1 fib2)))))
+
+:spades
+:diamonds
+:hearts
+:clubs
+1 13
+
+{:suit :spades
+ :value 1}
+
+;; lists for stacks
+
+{:draw ()
+
+ :discard ()
+
+ :spades ()
+ :clubs ()
+ :diamonds ()
+ :hearts ()
+
+ :lines [(list {:suit :spades :value 1}) (list {:suit :hearts :value 9})]
+ :stacks [() (list {:suit :clubs :value 5})]
+}
+
+(defn win? [game-state]
+  (= 52 (+ (count (:spades game-state))
+           (count (:clubs game-state))
+           (count (:diamonds game-state))
+           (count (:hearts game-state)))))
+
+(defn deck-take [n deck]
+  [(take n deck) (drop n deck)])
+
+(def suits [:spades :clubs :diamonds :hearts])
+(defn generate-card [s v] {:suit s :value v})
+
+(defn generate-deck []
+  (for [s suits
+        v (range 1 14)]
+      (generate-card s v)))
+
+(defn new-game []
+  (let [deck (generate-deck)
+        [lines deck] (loop [deck deck
+                            accum []]
+                       (if (= 7 (count accum))
+                         [accum deck]
+                         (let [[line deck] (deck-take (count accum) deck)]
+                           (recur deck (conj accum line)))))
+
+(vec (for [x (range 7)]
+                     (take x deck)))
+        
+    {:draw ()
+
+     :discard ()
+
+     :spades ()
+     :clubs ()
+     :diamonds ()
+     :hearts ()
+
+     :lines [(list {:suit :spades :value 1}) (list {:suit :hearts :value 9})]
+     :stacks [() (list {:suit :clubs :value 5})]}))
